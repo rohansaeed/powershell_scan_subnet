e659650dbfef72e213000b175fef4a1183635f4c
rohansaeed <67601569+rohansaeed@users.noreply.github.com>
2020-07-06 11:16:10 -0400

    Update ping_scan.ps1

--- ping_scan.ps1
+++ ping_scan.ps1
@@ -1,1 +1,1 @@
- 1..255 | % {ping -n 1 -w 100 192.168.1.$_} | Select-String ttl | % {$_ -Replace '^Reply from |: bytes=.+'} | % {echo "$_, up"} > online_hosts.csv
+ 1..255 | % {ping -n 1 -w 100 192.168.1.$_} | Select-String ttl | % {$_ -Replace '^Reply from |: bytes=.+'} | % {echo "$_,up,$(Get-Date -Format d),$(Get-Date -Format hh:mm:ss)"} > online_hosts.csv
